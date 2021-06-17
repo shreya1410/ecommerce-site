46ab576fddcd436310d9fd12d4d6ab79f5137f08
shreya1410 <shreyaraval415@gmail.com>
2021-06-17 15:48:32 +0530

    some changes

--- resources/views/admin/a_layout/head.blade.php
+++ resources/views/admin/a_layout/head.blade.php
@@ -14,18 +14,19 @@
 <!-- AdminLTE Skins. Choose a skin from the css/skins
      folder instead of downloading all of them to reduce the load. -->
 <link rel="stylesheet" href="{{asset('admin/dist/css/skins/_all-skins.min.css')}}">
-<!-- iCheck -->
-<link rel="stylesheet" href="{{asset('admin/plugins/iCheck/flat/blue.css')}}">
-<!-- Morris chart -->
-<link rel="stylesheet" href="{{asset('admin/plugins/morris/morris.css')}}">
-<!-- jvectormap -->
-<link rel="stylesheet" href="{{asset('admin/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
+<!-- bootstrap wysihtml5 - text editor -->
+<link rel="stylesheet" href="{{asset('admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
 <!-- Date Picker -->
 <link rel="stylesheet" href="{{asset('admin/plugins/datepicker/datepicker3.css')}}">
 <!-- Daterange picker -->
 <link rel="stylesheet" href="{{asset('admin/plugins/daterangepicker/daterangepicker.css')}}">
-<!-- bootstrap wysihtml5 - text editor -->
-<link rel="stylesheet" href="{{asset('admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{asset('admin/plugins/iCheck/flat/blue.css')}}">
+<!-- jvectormap -->
+<link rel="stylesheet" href="{{asset('admin/plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
+<!-- Morris chart -->
+<link rel="stylesheet" href="{{asset('admin/plugins/morris/morris.css')}}">
+
 
 @section('headSection')
 @show
